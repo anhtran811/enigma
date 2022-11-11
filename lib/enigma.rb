8f6c844f2f3c552f73c992b684fd78f65587872a
Anhnhi Tran <anhtran811@gmail.com>
2022-11-11 10:38:18 -0700

    Got shift test to pass

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -37,6 +37,12 @@ class Enigma
   end
 
   def shift(keys, offsets)
+    shift_hash = {}
+    shift_hash[:A] = keys[0].to_i + offsets[0]
+    shift_hash[:B] = keys[1].to_i + offsets[1]
+    shift_hash[:C] = keys[2].to_i + offsets[2]
+    shift_hash[:D] = keys[3].to_i + offsets[3]
+    shift_hash
   end
 
   def encrpyt
