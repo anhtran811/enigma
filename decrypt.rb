020c35df30f5973cd9a4d8d6685da539f4c6b3a8
Anhnhi Tran <anhtran811@gmail.com>
2022-11-13 14:43:19 -0700

    Got decrypt file i/o to work

--- decrypt.rb
+++ decrypt.rb
@@ -1,9 +1,17 @@
-# Additionally, you should create a Runner file called decrypt.rb that takes 
-# four command line arguments. The first is an existing file that 
-# contains an encrypted message. 
-# The second is a file where your program should write the decrypted message. 
-# The third is the key to be used for decryption. 
-# The fourth is the date to be used for decryption. 
-# In addition to writing the decrypted message to the file, 
-# your program should output to the screen the file it wrote to, 
-# the key used for decryption, and the date used for decryption.
+require './lib/enigma'
+require 'date'
+
+enigma = Enigma.new
+
+encrypted_file = File.open(ARGV[0], 'r')
+encrypted_message = encrypted_file.read
+encrypted_file.close
+
+decrypted_message = enigma.decrypt(encrypted_message.chomp, ARGV[2], ARGV[3])
+
+decryption = File.open(ARGV[1], 'w')
+decryption.write(decrypted_message[:decryption])
+decryption.close
+
+puts "Created '#{ARGV[1]}' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
+
